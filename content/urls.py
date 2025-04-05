43b58328436a946545797893dcebe36f2baba566
Aynal <aynalhaque7411@gmail.com>
2025-04-05 02:24:18 -0700

    two file

--- content/urls.py
+++ content/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('language/<str:language>/', views.language_overview, name='language_overview'),
     path('language/<str:language>/tutorials/', views.tutorial_list, name='language_tutorials'),
     path('category/<slug:category_slug>/', views.tutorial_list, name='category_tutorials'),
+    path('about_us/', views.about_us, name='about_us'),
 ]
